ff317c8ccedc02a9cafb0d8a89b60aa01a04a6dc
Valeriia Mormul <152411242+ValeriaMormul@users.noreply.github.com>
2023-12-04 19:31:44 +0200

    Добавьте файлы проекта.

---
+++ Task2.3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение дохода за год: ");
+        double annualIncome = Convert.ToDouble(Console.ReadLine());
+
+        // Визначення ставок податку для різних діапазонів річного доходу
+        double taxRate1 = 0.1;
+        double taxRate2 = 0.15;
+        double taxRate3 = 0.2;
+
+        double tax = 0;
+        if (annualIncome <= 50000)
+        {
+            tax = annualIncome * taxRate1;
+        }
+        else if (annualIncome <= 100000)
+        {
+            tax = 50000 * taxRate1 + (annualIncome - 50000) * taxRate2;
+        }
+        else
+        {
+            tax = 50000 * taxRate1 + 50000 * taxRate2 + (annualIncome - 100000) * taxRate3;
+        }
+        Console.WriteLine($"Податок на дохід: {tax}");
+    }
+}
